5e5a04c01d51ee164022f7288caca51d522965ce
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-07-01 18:29:50 +0530

    added isLiked to post

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -147,7 +147,18 @@ const likePostController = async (req, res) => {
 const getFeedPostController = async (req, res) => {
   const username = req.user.username;
 
-  const posts = await postModel.find().populate("user", "-password");
+  const posts = await Promise.all(
+    (await postModel.find({}).populate("user", "-password").lean()).map(
+      async (post) => {
+        const isLiked = await likeModel.findOne({
+          user: username,
+          post: post._id,
+        });
+        post.isLiked = Boolean(isLiked);
+        return post;
+      },
+    ),
+  );
 
   res.status(200).json({
     message: "Feed fetched successfully",
